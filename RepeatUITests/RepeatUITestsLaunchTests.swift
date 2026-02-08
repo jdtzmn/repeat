e8b40a5a18b9edee8ff36f9e03a02fde999b2d7a
Jacob Daitzman <jdtzmn@gmail.com>
2026-02-07 19:33:37 -0500

    Fix remaining UI test lint warnings

--- RepeatUITests/RepeatUITestsLaunchTests.swift
+++ RepeatUITests/RepeatUITestsLaunchTests.swift
@@ -8,6 +8,7 @@
 import XCTest
 
 final class RepeatUITestsLaunchTests: XCTestCase {
+    // swiftlint:disable:next static_over_final_class
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
